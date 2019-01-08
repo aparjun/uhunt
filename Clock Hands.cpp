75d58382a4e6f1c3bf09f7f547dd0c4018523792
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-08 11:14:59 +0530

    Update and rename clock.cpp to Clock Hands.cpp

---
+++ Clock Hands.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	string str;
+	while(1)
+	{
+	    double a=0,b=0,c=0;	 
+	    cin>>str;
+	    if(str.size()==4 && str[0]=='0' && str[2]=='0' && str[3]=='0')
+	    {
+	 	   break;
+	    }
+	    if(str.size()==5)
+	    { 
+	     b=str[3]-'0';	
+	     c=str[4]-'0';
+	    }
+	    else
+	    {
+	     b=str[2]-'0';	
+	     c=str[3]-'0';
+	    }  
+		a=stod(str);
+		b=b*10;
+		b=b+c;
+		b=b/60;
+		a=a+b;
+		a=(a*360)/12;
+		b=(b*360);
+		c=a-b;
+		if(c<=180)
+		{
+		 printf("%.3lf",c);
+		 printf("\n");
+		}
+		else
+		{
+		 printf("%.3lf",360-c);
+		 printf("\n");
+		}
+	} 
+	return 0;
+}
