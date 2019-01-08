adfc29655d16599743729dab59028115532c1bd6
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-08 09:16:09 +0530

    Update and rename minesweeper.cpp to Minesweeper.cpp

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -3,17 +3,26 @@ using namespace std;
 
 int main() 
 {
+	int n=1;
 	while(1)
 	{
 	 int r,c;
 	 cin>>r;
 	 cin>>c;
-	 char a1[r][c],b[r][c];
+	 char a1[r][c];
 	 int a2[100][100]={0};
-	 if(r==0 || c==0)
+	 if(r==0)
 	 {
 	 	break;
 	 }
+	 else
+	 {
+	 	if(n>1)
+	 	{
+	 		cout<<"\n";
+	 	}
+	 }
+	 cout<<"Field #"<<n<<":"<<"\n";
 	 for(int j=0;j<r;j++)
 	 {
 	 	for(int k=0;k<c;k++)
@@ -21,46 +30,37 @@ int main()
 	 		cin>>a1[j][k];
 	 		if(a1[j][k]=='*')
 	 		{
-	 			b[j][k]='*';
 	 			if(j-1>=0)
 	 			{
 	 			 a2[j-1][k]++;	
-	 			 b[j-1][k]=a2[j-1][k];
 	 			}
 	 			if(k-1>=0)
 	 			{
 	 			 a2[j][k-1]++;	
-	 			 b[j][k-1]=a2[j][k-1];
 	 			}
 	 			if(j-1>=0 && k-1>=0)
 	 			{
 	 			 a2[j-1][k-1]++;
-	 			 b[j-1][k-1]=a2[j-1][k-1];
 	 			}
 	 			if(j+1<r)
 	 			{
 	 			 a2[j+1][k]++;	
-	 			 b[j+1][k]=a2[j+1][k];
 	 			}
 	 			if(k+1<c)
 	 			{
 	 			 a2[j][k+1]++;
-	 			 b[j][k+1]=a2[j][k+1];
 	 			}
 	 			if(j+1<r && k+1<c)
 	 			{
 	 			 a2[j+1][k+1]++;
-	 			 b[j+1][k+1]=a2[j+1][k+1];
 	 			}
 	 			if(j+1<r && k-1>=0)
 	 			{
 	 			 a2[j+1][k-1]++;	
-	 			 b[j+1][k-1]=a2[j+1][k-1];
 	 			}
 	 			if(k+1<c && j-1>=0)
 	 			{
 	 			 a2[j-1][k+1]++;
-	 			 b[j-1][k+1]=a2[j-1][k+1];
 	 			}
 	 		}
 	 	}
@@ -69,11 +69,18 @@ int main()
 	 {
 	 	for(int k=0;k<c;k++)
 	 	{
-	 		cout<<b[j][k]<<" ";
+	 		if(a1[j][k]=='*')
+	 		{
+	 			cout<<"*";
+	 		}
+	 		else
+	 		{
+	 			cout<<a2[j][k];
+	 		}
 	 	}
 	 	cout<<"\n";
 	 }
-	 cout<<"\n";
+	 n++;
 	}
 	return 0;
 }
