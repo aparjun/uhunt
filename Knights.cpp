aa88a9d1426226dae57ee0ed9d74c50ab503c16f
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-09 16:01:56 +0530

    Update and rename Knight.cpp to Knights.cpp

---
+++ Knights.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<algorithm> 
+using namespace std;
+
+int main()
+{
+	while(1)
+	{
+	   int r,c,m,i,n=0;
+	   cin>>r;
+	   cin>>c;
+	   if(r==0 && c==0)
+	   {
+	   	break;
+	   }
+	   m=max(r,c);
+	   i=min(r,c);
+	   if(i!=0)
+	   {
+	     if(r<3 || c<3)
+	     {
+	   	  if(r==1 || c==1)
+	   	  {
+	   		  n=m;
+	   	  }
+	   	  else
+	   	  {
+	   		  if(m%2==0)
+	   		  {
+	   			  if(m%4!=0)
+	   			  {
+	   				  n=m+2;
+	   			  }
+	   			  else
+	   			  {
+	   				  n=m;
+	   			  }
+	   		  }
+	   		  else
+	   		  {
+	   			  n=m+1;
+	   		  }
+	   	  }
+	     }
+	     else
+	     {
+	   	   n=r*c;
+	       if(n%2!=0)
+	       {
+	   	    n=n+1;
+	       }
+	       n=n/2;
+	     }
+	   }
+	   cout<<n<<" knights may be placed on a "<<r<<" row "<<c<<" column board.\n";
+	}   
+	return 0;
+}
