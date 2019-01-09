b3bd81d3e735af77d681fb54f124812217a89a93
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-09 15:47:27 +0530

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -1,31 +1,54 @@
 #include <iostream>
+#include<algorithm> 
 using namespace std;
 
 int main()
 {
 	while(1)
 	{
-	   int r,c,n;
+	   int r,c,m,n;
 	   cin>>r;
 	   cin>>c;
 	   if(r==0 && c==0)
 	   {
 	   	break;
 	   }
-	   n=r*c;
-	   if(n==2 || n==6)
+	   m=max(r,c);
+	   if(r<3 || c<3)
 	   {
-	    n=n+1;
-	   } 
-	   if(n==4)
-	   {
-	   	n=n+4;
+	   	if(r==1 || c==1)
+	   	{
+	   		n=m;
+	   	}
+	   	else
+	   	{
+	   		if(m%2==0)
+	   		{
+	   			if(m%4!=0)
+	   			{
+	   				n=m+2;
+	   			}
+	   			else
+	   			{
+	   				n=m;
+	   			}
+	   		}
+	   		else
+	   		{
+	   			n=m+1;
+	   		}
+	   	}
 	   }
-	   if(n%2!=0)
+	   else
 	   {
-	   	n=n+1;
+	   	 n=r*c;
+	     if(n%2!=0)
+	     {
+	   	  n=n+1;
+	     }
+	     n=n/2;
 	   }
-	   n=n/2;
+	  
 	   cout<<n<<" knights may be placed on a "<<r<<" row "<<c<<" column board.\n";
 	}   
 	return 0;
