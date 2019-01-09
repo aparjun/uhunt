edaf2beff4d65ee65eb662657e7c1bd0eac86dde
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-09 12:20:39 +0530

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -5,110 +5,28 @@ int main()
 {
 	while(1)
 	{
-	   int r,c,n=0;
+	   int r,c,n;
 	   cin>>r;
 	   cin>>c;
-	   if(r==0)
+	   if(r==0 && c==0)
 	   {
 	   	break;
 	   }
-	   int ar[500][500]={0};
-	   for(int j=0;j<r;j++)
+	   n=r*c;
+	   if(n==2 || n==6)
 	   {
-	   	for(int k=0;k<c;k++)
-	   	{
-	   		if(ar[j][k]=='0')
-	 		{
-	 			if(j+1<r && k-2>=0)
-	 			{
-	 			 if(a2[j+1][k-2]=='0')
-	 			 {
-	 			  a2[j+1][k-2]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }
-	 			}
-	 			if(k-1>=0 && j-2>=0)
-	 			{
-	 			 if(a2[j-2][k-1]=='0')
-	 			 {
-	 			  a2[j-2][k-1]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }	
-	 			}
-	 			if(j-2>=0 && k+1<c)
-	 			{
-	 			 if(a2[j-2][k+1]=='0')
-	 			 {
-	 			  a2[j-2][k+1]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }
-	 			}
-	 			if(j-1>=0 && k+2<c)
-	 			{
-	 			 if(a2[j-1][k+2]=='0')
-	 			 {
-	 			  a2[j-1][k+2]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }	
-	 			}
-	 			if(k+1<c && j+2<r)
-	 			{
-	 			 if(a2[j+2][k+1]=='0')
-	 			 {
-	 			  a2[j+2][k+1]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }
-	 			}
-	 			if(j-2>=0 && k+1<c)
-	 			{
-	 			 if(a2[j-2][k+1]=='0')
-	 			 {
-	 			  a2[j-2][k+1]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }
-	 			}
-	 			if(j-2>=0 && k-1>=0)
-	 			{
-	 			 if(a2[j-2][k-1]=='0')
-	 			 {
-	 			  a2[j-2][k-1]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }	
-	 			}
-	 			if(k-2>=0 && j-1>=0)
-	 			{
-	 			 if(a2[j-1][k-2]=='0')
-	 			 {
-	 			  a2[j-1][k-2]++;
-	 			 }
-	 			 else
-	 			 {
-	 			 	break;
-	 			 }
-	 			}
-	   	    }
+	    n=n+1;
+	   } 
+	   if(n==4)
+	   {
+	   	n=n+4;
 	   }
+	   if(n%2!=0)
+	   {
+	   	n=n+1;
+	   }
+	   n=n/2;
+	   cout<<n<<" knights may be placed on a "<<r<<" row "<<c<<" column board.\n";
 	}   
 	return 0;
 }
