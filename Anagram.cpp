48ad62497c9b640935eb1ba50c01a237b0d7178e
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-09 10:27:01 +0530

    Update Anagram.cpp

--- Anagram.cpp
+++ Anagram.cpp
@@ -3,6 +3,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool compare(char a, char b)
+{
+	if(tolower(a) == tolower(b))
+	{
+		if(a<b)
+		{
+			return true;
+		}
+	}
+	if(tolower(a) < tolower(b))
+	{
+	 return true;
+	} 
+	return false;
+}
 int main() 
 {
 	int t,i=0;
@@ -10,44 +25,12 @@ int main()
 	string str;
 	while(i<t)
 	{
-		string s[10000];
-		int n=0;
 		cin>>str;
-		sort(str.begin(),str.end());
+		sort(str.begin(),str.end(),compare);
 		do{
-		s[n]=str;
-		n++;
-		}while(next_permutation(str.begin(),str.end()));
-		for(int j=0;j<n;j++)
-	    {
-	       for(int k=0;k<n;k++)
-	       {
-		    if(j!=k && s[j]==s[k])
-		    {
-			  s[j]="123";
-			  s[k]="123";
-		    }
-	       }	
-	    }
-	    for(int j=0;j<n-1;j++)
-	    {
-	      for(int k=j+1;k<=n-1;k++)
-	      {
-		   if(s[k]>s[j])
-		   {
-			str=s[j];
-			s[j]=s[k];
-			s[k]=str;
-		   } 
-	      }	
-	    }
-	    for(int j=n-1;j>=0;j--)
-	    {
-		 if(isalpha(s[j][0]))
-		 {
-		  cout<<s[j]<<"\n";
-		 } 
-	    }
+		cout<<str<<"\n";
+		}while(next_permutation(str.begin(),str.end(),compare));
+		
 		i++;
 	}
 	return 0;
