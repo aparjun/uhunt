7d502388c57c32a6ea563292fe42cf4c4f081d6a
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-09 09:39:08 +0530

    Update and rename Hangman.cpp to Hangman Judge.cpp

--- Hangman Judge.cpp
+++ Hangman Judge.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main() 
 {
-	int r,n=0;
+	int r;
 	while(cin>>r)
 	{
 		if(r==-1)
 		{
 			break;
 		}
-		n++;
-		int a=0,b=0,c=0;
-		string str,abc;
+		int a=0,b=0,c=0,d=0;
+		string str,cba,abc="";
 		cin>>str;
-		cin>>abc;
-		cout<<"Round "<<n<<"\n";
+		cin>>cba;
+		abc+=cba[0];
+		for(int k=1;k<cba.size();k++)
+		{
+			for(int l=0;l<abc.size();l++)
+			{
+			 if(cba[k]==abc[l])
+			 {
+			 	d++;
+			 }
+			}
+			if(d==0)
+			 {
+			 	abc+=cba[k];
+			 }
+			 d=0;
+		}
+		cout<<"Round "<<r<<"\n";
 		for(int j=0;j<abc.size();j++)
 		{
 			for(int k=0;k<str.size();k++)
 			{
 				if(abc[j]==str[k])
 				{
-					str[k]='0';
 					a++;
 					c++;
 				}
-				if(a==str.size())
+			}
+			if(a==str.size())
 				{
 					cout<<"You win.\n";
 					break;
 				}
-			}
 			if(c==0)
 			{
 				b++;
