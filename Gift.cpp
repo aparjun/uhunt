656c14a34fa30b6c1f71aca1cdca2d9afe22d279
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-11 16:54:17 +0530

    Add files via upload

---
+++ Gift.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	while(cin>>t)
+	{
+	 int a,b;
+	 string str;
+	 string s[t];
+	 int a1[t]={0};
+	 int a2[t]={0};
+	 for(int i=0;i<t;i++)
+	 {
+	 	cin>>s[i];
+	 }
+	 for(int i=0;i<t;i++)
+	 {
+	 	cin>>str;
+	 	cin>>a;
+	 	cin>>b;
+	 	a1[]
+	 	for(int j=0;j<b;j++)
+	 	{
+	 		cin>>str;
+	 		for()
+	 	}
+	 }
+	}
+	return 0;
+}
