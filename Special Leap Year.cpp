d0a67f996580d3eefeba6420af3d999bbaf5cea5
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-23 11:09:08 +0530

    Update Special Leap Year.cpp

--- Special Leap Year.cpp
+++ Special Leap Year.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main() 
 {
 	string str;
-	while(cin>>str)
-	{
-		int a=0,b=0,c=0,d=0,e=0,f=0,h=0;
+	cin>>str;
+	int a=0,b=0,c=0,d=0,e=0,f=0,h=0;
 		a=str[str.size()-1]-'0';
 		b=str[str.size()-2]-'0';
 		c=str[str.size()-3]-'0';
@@ -16,6 +15,7 @@ int main()
 		d=d*1000;
 		c=a+b+c+d;
 		a=a+b;
+		
 		int flag=0,g=0;
 		if(a%4==0 && ((a%25==0 && c%16==0) || a!=0))
   		{
@@ -54,7 +54,58 @@ int main()
         {
         	cout<<"This is an ordinary year.\n";
         }
-        cout<<"\n";
+	while(cin>>str)
+	{
+		cout<<"\n";
+	    a=0,b=0,c=0,d=0,e=0,f=0,h=0;
+		a=str[str.size()-1]-'0';
+		b=str[str.size()-2]-'0';
+		c=str[str.size()-3]-'0';
+		d=str[str.size()-4]-'0';
+		b=b*10;
+		c=c*100;
+		d=d*1000;
+		c=a+b+c+d;
+		a=a+b;
+		
+		flag=0,g=0;
+		if(a%4==0 && ((a%25==0 && c%16==0) || a!=0))
+  		{
+  		 cout<<"This is leap year.\n";
+  		 for(int j=0;j<str.size();j++)
+  		 {
+  		 	if(j%2==0)
+  		 	{
+  		 		e=e+(str[j]-'0');
+  		 	}
+  		 	else
+  		 	{
+  		 		e=e-(str[j]-'0');
+  		 	}
+  		 }
+  		 if(e%11==0 && (str[str.size()-1]=='0' || str[str.size()-1]=='5'))
+  		 {
+  		 	g++;
+  		 }
+  		 flag++;
+  		}
+  		for(int j=0;j<str.size();j++)
+  		 {
+  		 		h=h+(str[j]-'0');
+  		 }
+  		if(h%3==0 && (str[str.size()-1]=='0' || str[str.size()-1]=='5'))
+  		{
+  			cout<<"This is huluculu festival year.\n";
+  			flag++;
+  		}
+  		if(g!=0)
+  		{
+  			cout<<"This is bulukulu festival year.\n";
+  		}
+        if(flag==0)
+        {
+        	cout<<"This is an ordinary year.\n";
+        }
 	}
 	return 0;
 }
