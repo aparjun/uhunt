9fd2a210271794ff85252652e93c0a9b96feb932
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-24 11:21:44 +0530

    Update Stack Rail.cpp

--- Stack Rail.cpp
+++ Stack Rail.cpp
@@ -7,48 +7,53 @@ int main()
 	int n;
 	while(cin>>n)
 	{
-		stack <int> s;
 		if(n==0)
 		{
 			break;
 		}
-		int a[n],i=0;
-		while(i<n)
+		int a[n],f=0;
+		while(f==0)
 		{
-			cin>>a[i];
-			if(a[i]==0)
-			{
-				cout<<"\n";
-				break;
-			}
-			i++;
-		}
-		i=1;
-	    for(int j=0;j<n;j++)
-	    {
-		 while(i<=n && (s.empty() || s.top()!=a[j]))
+		 for(int j=0;j<n;j++)
 		 {
-			 s.push(i);	
-			 i++;
+		 	cin>>a[j];
+		 	if(a[j]==0)
+		 	{
+		 		cout<<"\n";
+		 		f++;
+		 		break;
+		 	}
 		 }
-		 if(s.top()==a[j])
+		 if(f==0)
 		 {
-			 s.pop();
-		 } 
-		 else
-		 {
-		 	break;
+		 	stack <int> s;
+		    int i=1;
+	        for(int j=0;j<n;j++)
+	        {
+		        while(i<=n && (s.empty() || s.top()!=a[j]))
+		        {
+			     s.push(i);	
+			     i++;
+		        }
+		        if(s.top()==a[j])
+		        {
+			      s.pop();
+		        } 
+		        else
+		        {
+		 	     break;
+		        }
+	        }
+	        if(s.empty())
+	        {
+	     	 cout<<"Yes\n";
+	        }
+	        else
+	        {
+	     	 cout<<"No\n";
+	        }
 		 }
 	    } 
-	     if(s.empty())
-	     {
-	     	cout<<"Yes\n";
-	     }
-	     else
-	     {
-	     	cout<<"No\n";
-	     }
 	}
 	return 0;
 }
-
