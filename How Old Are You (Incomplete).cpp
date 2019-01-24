6451a437751600b53017e7dde61738cdcc50b099
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-24 14:44:25 +0530

    Update How Old Are You (Incomplete).cpp

--- How Old Are You (Incomplete).cpp
+++ How Old Are You (Incomplete).cpp
@@ -8,7 +8,8 @@ int main()
   cin>>t;
   while(i<=t)
   {
-  	int f=0,c=0;
+  	int f=0;
+  	float a=0,b=0,c=0;
   	string str,abc;
   	cin>>str;
   	cin>>abc;
@@ -19,6 +20,7 @@ int main()
   	m2=stoi(abc.substr(3,5));
   	y1=stoi(str.substr(6,10));
   	y2=stoi(abc.substr(6,10));
+  	cout<<"Case #"<<i<<": ";
   	if(y2>=y1)
   	{
   		if(y2>y1)
@@ -56,58 +58,13 @@ int main()
   	}
   	if(f==0)
   	{
-  		   while(1)
-  	       {
-  			d2++;
-  			if(d1==d2 && m1==m2 && y1==y2)
-  			{
-  				break;
-  			}
-  			if(m2==1 || m2==3 || m2==5 || m2==7 || m2==8 || m2==10 || m2==12)
-  			{
-  			 if(d2>31)
-  			 {
-  			 	d2=1;
-  			 	m2++;
-  			 	if(m2>12)
-  			 	{
-  			 		m2=1;
-  			 		y2++;
-  			 		c++;
-  			 	}
-  			 }
-  			}
-  			else
-  			{
-  				if(m2==2)
-  				{
-  				 if(y2%4==0 && (y2%400==0 || y2%100!=0))
-  				 {
-  				 	if(d2>29)
-  				 	{
-  				 		d2=1;
-  				 		m2++;
-  				 	}
-  				 }
-  				 else
-  				 {
-  				   if(d2>28)
-  				   {
-  				   	d2=1;
-  				   	m2++;
-  				   }
-  				 }
-  				}
-  				else
-  				{
-  					if(d2>30)
-  					{
-  						d2=1;
-  						m2++;
-  					}
-  				}
-  			}
-           }
+  		   c=y1-y2;
+  		   b=m1-m2;
+  		   b=b/12;
+  		   a=d1-d2;
+  		   a=a/365;
+  		   c=c+a+b;
+  		   c=(int)c;
            if(c>130)
            {
            	cout<<"Check birth date\n";
