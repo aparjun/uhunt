9c28aab609db6bcf6fefa16df560047ae1cc6ced
aparjun <46344050+aparjun@users.noreply.github.com>
2019-01-28 14:45:05 +0530

    Update Newspaper.cpp

--- Newspaper.cpp
+++ Newspaper.cpp
@@ -5,32 +5,44 @@ using namespace std;
 int main()
 {
 	int t,i=0;
+	cin>>t;
 	while(i<t)
 	{ 
+	 string str;
+	 float n; 	
 	 char a;
-	 int k,l,m,ar[127]={0},br[127]={0};
+	 int k,l,m,ar[500]={0},br[500]={0};
 	 cin>>k;
 	 for(int j=0;j<k;j++)
 	 {
 	 	cin>>a;
-	 	l=a-'0';
+	 	l=a;
 	 	cin>>m;
 	 	ar[l]=m;
 	 }
 	 cin>>k;
-	 while(cin>>a)
+	 getline(cin,str);
+	 for(int j=0;j<k;j++)
 	 {
-	 	if(a!=' ')
+	 	getline(cin,str);
+	 	for(int f=0;f<str.size();f++)
 	 	{
-	 	  l=a-'0';
-	 	  br[l]++;
+	 		a=str[f];
+	 		if(a!=' ')
+	 	    {
+	 	     l=a;
+	 	     br[l]++;
+	 	    }
 	 	}
 	 }
-	 for(int j=0;j<127;j++)
+	 for(int j=0;j<500;j++)
 	 {
-	 	br[j]=br[j]*ar[j];
+	  br[j]=br[j]*ar[j];
 	 }
-	 cout<<accumulate(br , br+127, 0)<<endl; 
+	 n=accumulate(br, br+500, 0);
+	 n=n/100;
+	 printf("%.2f",n);
+	 cout<<"$"<<endl;
 		i++;
 	}
 	return 0;
