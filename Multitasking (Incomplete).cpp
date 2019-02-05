3111da5c2e60e63babca1016eb8620f025269a60
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-05 14:42:51 +0530

    Add files via upload

---
+++ Multitasking (Incomplete).cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,m;
+	while(cin>>n>>m)
+	{
+		
+		if(n==0 && m==0)
+		{
+			break;
+		}
+		int ar[1000100]={0};
+		int a,b,c,f=0;
+		for(int j=0;j<n;j++)
+		{
+			cin>>a>>b;
+			for(int i=a;i<=b;i++)
+			{
+				ar[i]++;
+				if(ar[i]>1 && i!=a && i!=b)
+				{
+					f=1;
+					break;
+				}
+			}
+		}
+		for(int j=0;j<m;j++)
+		{
+			cin>>a>>b>>c;
+			while(a<1000000)
+			{
+			  for(int i=a;i<=b && i<=1000000;i++)
+			  {
+				ar[i]++;
+				if(ar[i]>1 && i!=a && i!=b)
+				{
+					f=1;
+					break;
+				}
+			  }	
+			  a=a+c;
+			  b=b+c;
+			}
+		}
+		
+		if(f==0)
+		{
+			cout<<"NO CONFLICT\n";
+		}
+		else
+		{
+			cout<<"CONFLICT\n";
+		}
+	}
+	return 0;
+}
