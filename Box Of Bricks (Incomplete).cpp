757c6f881845d6e282a87c62f109e5408ad74c04
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-06 16:54:34 +0530

    Add files via upload

---
+++ Box Of Bricks (Incomplete).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n,i=0;
+	while(cin>>n)
+	{
+		i++;
+		if(n==0)
+		{
+			break;
+		}
+		int ar[n];
+		int sum=0,c=0,d=0;
+		for(int j=0;j<n;j++)
+		{
+			cin>>ar[j];
+			sum=sum+ar[j];
+		}
+		sum=sum/n;
+		cout<<sum;
+		
+		for(int j=0;j<n;j++)
+		{
+			if(ar[j]>sum)
+			{
+				c=c+ar[j]-sum;
+			}
+			else
+			{
+				d=d+sum-ar[j];
+			}
+		}
+		if(c>d)
+		{
+		 cout<<"Set #"<<i<<"\n";
+		 cout<<"The minimum number of moves is "<<c<<"\n\n";
+		}
+		else
+		{
+		 cout<<"Set #"<<i<<"\n";
+		 cout<<"The minimum number of moves is "<<d<<"\n\n";	
+		}
+	}
+	return 0;
+}
