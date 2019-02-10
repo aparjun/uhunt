de420e98ba852fa0e6532831a9343a3fb83d9e37
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-10 21:23:39 +0530

    Update Unique Snowflakes (Incomplete).cpp

--- Unique Snowflakes (Incomplete).cpp
+++ Unique Snowflakes (Incomplete).cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int t, n, x, ans, cnt, block;
+int t,n,x,ans,cnt,temp;
 map<int, int> m;
 int main() 
 {
@@ -12,18 +12,18 @@ int main()
     {
 	 cin>>n;
 	 m.clear();
-	 ans=0, cnt=0, block=0;
+	 ans=0, cnt=0, temp=0;
 	 for(int i=1;i<=n;i++)
 	 {
 	    cin>>x;
 	    int lx = m[x];
 	    if(lx != 0) 
 	    {
-		 if(block<lx)
+		 if(temp<lx)
 		 {
-		 	block=lx;
+		 	temp=lx;
 		 }
-		 cnt=i-block-1;
+		 cnt=i-temp-1;
 	    }
 	    cnt++;
 	    m[x]=i;
