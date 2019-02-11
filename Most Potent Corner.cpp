8813543f448865b59c2e3903fd4250c2ad57bf2f
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-11 16:47:19 +0530

    Update Most Potent Corner.cpp

--- Most Potent Corner.cpp
+++ Most Potent Corner.cpp
@@ -1,4 +1,5 @@
 #include<algorithm>
+#include<iostream>
 #include<cmath>
 #include<cstdio>
  
@@ -7,13 +8,13 @@ using namespace std;
 int n;
 int corners[20000];
 int sums[20000];
- 
+
 int main() 
 {
     while(cin>>n)
     {
         int num=pow(2, n);
-        int max=0,sum=0;
+        int b,max=0,sum=0;
         for(int i=0;i<num;i++)
         {
            cin>>corners[i];
@@ -22,23 +23,25 @@ int main()
         {
             sum=0;
             for(int j=0;j<n;j++)
-            {
-                int b=i^(1<<j);
-                sum+=corners[b];
+            {   
+            	b=i^(1<<j);  //if they are corners, only one coordinate will change
+                             //so xor of them gives a binary number with a single 1 (xor gives 0 if bits are same and 1 if two bits are different)
+                sum=sum+corners[b];
             }
             sums[i]=sum;
         }
         for(int i=0;i<num;i++)
         {
-            for(int j=0;j<n;j++) 
-            {
-                int b=i^(1<<j);
+            for(int j=0;j<n;j++)
+            {   
+            	b=i^(1<<j); 
                 if(max<sums[i]+sums[b])
                 {
-                	max=sums[i]+sums[b];
+                 max=sums[i]+sums[b];
                 }
             }
+            
         }
-        printf("%d\n", max);
+        cout<<max<<endl;
     }
 }
