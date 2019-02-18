b5715a6e6b388e3bc646f47004be2b7d3c6091b4
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-18 12:15:14 +0530

    Update Power Strings.cpp

--- Power Strings.cpp
+++ Power Strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() 
@@ -7,7 +8,7 @@ int main()
 	while(cin>>str && str!=".")
 	{
 		int r,s=str.size();
-		for(int i=0;i<str.size();i++)
+		for(int i=1;i<=s;i++)
 		{
 			string sub = str.substr(0, i);
 			string abc="";
