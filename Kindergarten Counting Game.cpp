a3eb69aeeb30459d3de9d8dd5573a84b49813515
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-18 11:19:42 +0530

    Update Kindergarten Counting Game.cpp

--- Kindergarten Counting Game.cpp
+++ Kindergarten Counting Game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<bits/stdc++.h> 
 using namespace std;
 
 int main() 
@@ -6,19 +7,32 @@ int main()
 	string str;
 	while(getline(cin,str))
 	{
-		int f=0,c=0;
 		for(int i=0;i<str.size();i++)
 		{
-			if(isalpha(str[i]))
+			if(!isalpha(str[i]))
 			{
-				f=1;
-			}
-			if(str[i]==' ' && f==0)
-			{
-				c++;
-				
+				str[i]=' ';
 			}
 		}
+		string w;
+		int f=0,c=0;
+		stringstream ss(str);
+		while(ss>>w)
+		{
+			for(int i=0;i<w.size();i++)
+			{
+				if(isalpha(w[i]))
+				{
+					f=1;
+				}
+			}
+			if(f==1)
+			{
+				c++;
+			}
+			f=0;
+		}
+		cout<<c<<endl;
 	}
 	return 0;
 }
