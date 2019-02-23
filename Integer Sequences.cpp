6f029f0781ea061bf3d218c9d87923e99a04fb65
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-23 10:59:41 +0530

    Add files via upload

---
+++ Integer Sequences.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+int main()
+{
+  int t,m,d,k,n;
+  long long c[30];
+  long long ans;
+  cin>>t;
+  while (t--) 
+  {
+    ans = 0;
+    cin>>m;
+    for(int i=0;i<=m;i++)
+    {
+      cin>>c[i];
+    }
+    cin>>d>>k;
+    for(n=1;n*n+n<2*k/d;n++);
+    {
+     for(int i=0;i<=m;i++)
+     {
+      ans+=c[i]*(long long)pow(n, i);
+     }
+    }
+    cout<<ans<<endl;
+  }
+  return 0;
+}
