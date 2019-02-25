4351599acf209ad421729e5e53d2963f3fb62acc
aparjun <46344050+aparjun@users.noreply.github.com>
2019-02-25 12:11:54 +0530

    Update Integer Sequences from Addition of Terms.cpp

--- Integer Sequences from Addition of Terms.cpp
+++ Integer Sequences from Addition of Terms.cpp
@@ -2,29 +2,31 @@
 #include <cmath>
 using namespace std;
 
-int main()
+int main() 
 {
-  int t,m,d,k,n;
-  long long c[30];
-  long long ans;
-  cin>>t;
-  while(t--) 
-  {
-    ans=0;
-    cin>>m;
-    for(int i=0;i<=m;i++)
-    {
-      cin>>c[i];
-    }
-    cin>>d>>k;
-    for(n=1;n*n+n<2*k/d;n++);
-    {
-     for(int i=0;i<=m;i++)
-     {
-      ans+=c[i]*(long long)pow(n, i);
-     }
-    }
-    cout<<ans<<endl;
-  }
-  return 0;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int m,d,k,ar[20],s=0;
+		cin>>m;
+		for(int i=0;i<=m;i++)
+		{
+			cin>>ar[i];
+		}
+		cin>>d>>k;
+		int p=0,prev=0,n=1;
+		while(prev<k && k<=p)
+		{
+			prev=p;
+            p+=n*d;
+            n++;
+		}
+		for(int i=0;i<=m;i++)
+		{
+			s=s+ar[i]*(int)pow(n,i);
+		}
+		cout<<s<<endl;
+	}
+	return 0;
 }
